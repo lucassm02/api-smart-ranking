436a3b73247e3148814318377efe92dd9daf97e1
Lucas Santos <lucassm02@gmail.com>
2021-05-30 23:10:16 +0000

    fix: corrected terminal configuration

--- .devcontainer/assets/entrypoint.sh
+++ .devcontainer/assets/entrypoint.sh
@@ -2,10 +2,8 @@
 
 # install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-#add config file
-mv -f /home/node/assets/.zshrc /home/node/
 #install spaceship theme
-git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt"
-ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
+git clone https://github.com/denysdovhan/spaceship-prompt.git "/home/node/.oh-my-zsh/custom/themes/spaceship-prompt"
+ln -s "/home/node/.oh-my-zsh/custom/themes/spaceship-prompt/spaceship.zsh-theme" "/home/node/.oh-my-zsh/custom/themes/spaceship.zsh-theme"
 #install plugins
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/zdharma/zinit/master/doc/install.sh)"
