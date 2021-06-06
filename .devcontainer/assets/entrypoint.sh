75af62a5f9bf76cb64fc9eebeba25ed7868b76b3
Lucas Santos <lucassm02@gmail.com>
2021-06-06 00:18:56 +0000

    chore: optimized devcontainer configuration

--- .devcontainer/assets/entrypoint.sh
+++ .devcontainer/assets/entrypoint.sh
@@ -1,9 +1,13 @@
 #!/bin/zsh
 
-# install oh-my-zsh
+WORKSPACE="/home/node" 
+
+#INSTALL OH-MY-ZSH
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-#install spaceship theme
-git clone https://github.com/denysdovhan/spaceship-prompt.git "/home/node/.oh-my-zsh/custom/themes/spaceship-prompt"
-ln -s "/home/node/.oh-my-zsh/custom/themes/spaceship-prompt/spaceship.zsh-theme" "/home/node/.oh-my-zsh/custom/themes/spaceship.zsh-theme"
-#install plugins
+
+#INSTALL SPACESHIP THEME
+git clone https://github.com/denysdovhan/spaceship-prompt.git "$WORKSPACE/.oh-my-zsh/custom/themes/spaceship-prompt"
+ln -s "$WORKSPACE/.oh-my-zsh/custom/themes/spaceship-prompt/spaceship.zsh-theme" "$WORKSPACE/.oh-my-zsh/custom/themes/spaceship.zsh-theme"
+
+#INSTALL PLUGINS
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/zdharma/zinit/master/doc/install.sh)"
